6505411f7a7facf464fd68163ba3e7e0568877e9
Barbacamanitu <russmbiz@gmail.com>
2016-07-17 20:18:13 -0500

    random generate
    t

--- src/RadixMain.java
+++ src/RadixMain.java
@@ -1,9 +1,20 @@
+import java.util.Random;
 
 public class RadixMain {
 
 	private static int[] numbers;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		numbers = new int[20];
+		
+		//Fill numbers with random
+		Random randGen = new Random();
+		for (int i = 0; i < 20; i++)
+		{
+			numbers[i] = randGen.nextInt(1000);
+		}
+		
+		int test = 1;
 		
 
 	}
